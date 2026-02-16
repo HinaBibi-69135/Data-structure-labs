6c2af5a7d2100f8696e997054cb60514c969eeaf
HinaBibi-69135 <69135@students.riphah.edu.pk>
2026-02-16 22:47:10 +0500

    Add files via upload

---
+++ DS Activity 3 Task 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char *ch = new char;
+
+    cout << "Enter a character: ";
+    cin >> *ch;
+
+    cout << "Stored character is: " << *ch << endl;
+
+    delete ch;
+
+    ch = nullptr;
+
+    return 0;
+}
+
